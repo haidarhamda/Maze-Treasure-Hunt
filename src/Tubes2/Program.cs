67c861048260414be4b91d549b982eecae9856e2
haidarhamda <haidarhamda23@gmail.com>
2023-03-19 17:33:40 +0700

    bfs

---
+++ src/Tubes2/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DefaultNamespace;
+using Tubes2;
+
+namespace Tes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"/home/suisei/Documents/GitHub/Tubes2_DuelisSejati/src/Tubes2/tes.txt");
+                string[,] map=new string[lines.GetLength(0),lines.ElementAt(0).Split(" ").Length];
+                for (int i = 0; i < lines.GetLength(0); i++)
+                {
+                    for (int j = 0; j < lines.ElementAt(i).Split(" ").Length; j++)
+                    {
+                        map[i,j] = lines.ElementAt(i).Split(" ").ElementAt(j);
+                    }
+                }
+                Map Map=new Map(map);
+                Map.showMap();
+                Bfs bfs = new Bfs(Map);
+                List<string> route=bfs.bfsearch();
+                for (int i = 0; i < route.Count; i++)
+                {
+                    Console.Write(route.ElementAt(i)+" ");
+                }
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+        }
+    }
+}
